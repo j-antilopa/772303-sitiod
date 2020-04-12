dd3a3981cfdb1508bd5402d79547119e74dfdf9f
plusqRED <rickes.oleg@gmail.com>
2020-04-12 15:21:13 +0300

    Lab 5 script

---
+++ main.R
@@ -0,0 +1,36 @@
+
+# Generate vector by using seq()
+a <- seq(1, 20, by=1)
+
+# Add number to vector
+a + 3
+
+# Mult number to vector
+a * 3
+
+# Sum of elements in the vector
+sum(a)
+
+# Max element in the vector
+max(a)
+
+# Min element in the vector
+min(a)
+
+# Returns the product of all the values present in its arguments.
+prod(a)
+
+# Get vector length
+length(a)
+
+# Calculate by taking the sum of the values and dividing with the number of values in a data series
+mean(a)
+
+# The variance is a numerical measure of how the data values is dispersed around the mean
+var(a)
+
+# Sorting the vector
+sort(a)
+
+# Vector math summary
+summary(a)
