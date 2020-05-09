2bb09a8bba0db8c01111962a0087189a77e1a2d5
kkkirill <kkg.410@mail.ru>
2020-05-10 00:11:12 +0300

    labs

---
+++ РоманенкоАВ/lab 5/lab5.R
@@ -0,0 +1,30 @@
+"lab 5"
+"Gen vector of 20 elements"
+a <- seq(1, 20, by = 1)
+"Add 2 to vector"
+a + 2
+"Minus 3 from vector"
+a - 3
+"Mult"
+a * 3
+"Into second power whole vector"
+a^2
+"sum of all elements of vector"
+sum(a)
+"max of vector"
+set.seed(42)
+max(sample(a))
+"min of vector"
+min(sample(a))
+"Prod of vector"
+prod(1:10)
+"Length of vector"
+length(a)
+"Mean of vector"
+mean(a)
+"Variancy of vector"
+var(a)
+"Sorted vector"
+sort(sample(a))
+"Summary of vector"
+summary(a)
